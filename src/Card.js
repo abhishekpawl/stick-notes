02222e72247c33daeeb954279f04c1f2d6dd60dc
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2021-09-16 21:42:52 +0530

    added form area

--- src/Card.js
+++ src/Card.js
@@ -16,6 +16,29 @@ const Card = () => {
         </div>
       </article>
 
+      <article className="card">
+
+        <form className="form">
+          
+          <div>
+            <label htmlFor="date">Date: </label>
+            <input type="date" id="date" name="date"></input>
+          </div>
+          <div>
+            <label htmlFor="note">Note:</label>
+            <input type="note" id="note" name="note"></input>
+          </div>
+          <div>
+            <button className="btn">Add note</button>
+          </div>
+          <div>
+            <button className="btn">Clear all</button>
+          </div>
+
+        </form>
+
+      </article>
+
     </React.Fragment>
   )
 }
